788dc2d31b7c674612564d3dfb01bb8e31c7fa0e
mauricio <mauriciocon@gmail.com>
2022-03-22 17:35:43 -0300

    Add: query to show all movies from database

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,15 +1,20 @@
-module.exports = server => {
+module.exports = server =>
+{
 
     const connection = require('../database/connection');
-    
-    server.get('/', (require, response) => {
-        response.json({message: 'Bem vindo ao catálogo de séries e filmes'})
+
+    server.get('/', (require, response) =>
+    {
+        response.json({ message: 'Bem vindo ao catálogo de séries e filmes' })
     })
 
-    server.get('/movies', (require, response) => {
-        const sql = '';
-        connection.query(sql, (error, res) => {
-            if (error){
+    server.get('/movies', (require, response) =>
+    {
+        const sql = 'SELECT * FROM movies';
+        connection.query(sql, (error, res) =>
+        {
+            if (error)
+            {
                 return error;
             } console.log("movies: ", res);
             response.json(res)
